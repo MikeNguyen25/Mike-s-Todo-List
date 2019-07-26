10c7197e21beab0182d424dddb9b0ff7c58fcfa4
Mike <manhphuc0205@gmail.com>
2019-07-26 18:14:56 +0700

    Initial Commit

---
+++ Mike's Todo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mike's Todo List
+//
+//  Created by Manh Phuc on 7/26/19.
+//  Copyright © 2019 Mike Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
